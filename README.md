f62e67df6a197329a9433fcc0f0c57b025d6d8d2
george <george.sattler@xvt.com.au>
2014-02-26 12:09:40 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ckanext-vdojtheme
+===========
+
+
---
+++ ckanext/vdojtheme/plugin.py
@@ -0,0 +1,17 @@
+import ckan.plugins as plugins
+
+
+
+class VDOJThemePluginClass(plugins.SingletonPlugin):
+    """
+    Setup plugin
+    """
+
+
+    plugins.implements(plugins.IConfigurer, inherit=True)
+
+    def update_config(self, config):
+        plugins.toolkit.add_template_directory(config, 'templates')
+        plugins.toolkit.add_public_directory(config, 'public')
+
+
